db928432610e8a6af52502400268c0e8d00f1045
Daniel Choi <choi.mina@gmail.com>
2019-02-24 04:05:51 -0500

    bolstered testing

--- src/server/main_test.go
+++ src/server/main_test.go
@@ -8,8 +8,41 @@ import (
 )
 
 func TestHandler(t *testing.T) {
+	// Form a new HTTP request
+	req, err := http.NewRequest("GET", "", nil)
+
+	// If we fail, stop the test
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// We use Go's httptest library to create an http recorder. This recorder
+	// will act as the target of our http request... like a mini-browser
+	recorder := httptest.NewRecorder()
+
+	// Create an HTTP handler from mainPage function
+	hf := http.HandlerFunc(mainPage)
+
+	// Serve the HTTP request to our recorder
+	hf.ServeHTTP(recorder, req)
+
+	// Check the status code is what we expect.
+	if status := recorder.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusOK)
+	}
+
+	// Check the response body is what we expect.
+	expected := `Hello World!`
+	if recorder.Body.String() != expected {
+		t.Errorf("handler returned unexpected body: got %v want %v", recorder.Body.String(), expected)
+	}
+
+}
+
+func TestRouter(t *testing.T) {
 	// Instantiate the router
-	r := newRouter()
+	r := newRouter() // this is from the main.go
 
 	// a mock server... this exposes the URL and making routing test possible
 	fakeServer := httptest.NewServer(r)
@@ -35,6 +68,34 @@ func TestHandler(t *testing.T) {
 	}
 
 	expected := "Hello World!"
+
+	if expected != string(byteD) {
+		t.Errorf("Response should be %s, got %s", expected, string(byteD))
+	}
+
+}
+
+func test404(t *testing.T) {
+	r := newRouter()
+	fakeServer := httptest.NewServer(r)
+	res, err := http.Post(fakeServer.URL+"/test", "", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Ensure 404 works...
+	if res.StatusCode != http.StatusMethodNotAllowed {
+		t.Errorf("Status code should 404, but got %d", res.StatusCode)
+	}
+
+	// Try to read the body... should be empty
+	byteD, err := ioutil.ReadAll(res.Body)
+	defer res.Body.Close()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	expected := ""
 
 	if expected != string(byteD) {
 		t.Errorf("Response should be %s, got %s", expected, string(byteD))
